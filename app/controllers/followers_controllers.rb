2b65e8bbf433eae43a1b4107b04ac1fbf473ca44
HAYASHInnn <fffm572752@gmail.com>
2025-07-17 14:26:46 +0900

    フォロー機能（POSTリクエストではなく、GETリクエストが送信されるエラーが発生している）

---
+++ app/controllers/followers_controllers.rb
@@ -0,0 +1,8 @@
+class FollowersController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        current_user.follow!(params[:account_id])
+        redirect_to account_path(params[:account_id])
+    end
+end
