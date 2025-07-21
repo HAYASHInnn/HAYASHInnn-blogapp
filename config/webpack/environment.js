002d66ff97ab109ea8a2eb2dd972d7fcccda740a
HAYASHInnn <fffm572752@gmail.com>
2025-07-21 22:29:16 +0900

    webpackerの設定を修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+environment.config.set('node', false);
+
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
+
+environment.config.merge(customConfig);
+
+module.exports = environment;
