98bfac365d1a5ef6ea0c7f74154135be426b397e
HAYASHInnn <fffm572752@gmail.com>
2025-07-21 22:39:55 +0900

    jQueryの動作確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 
+import "trix";
+import "@rails/actiontext";
 
-import "trix"
-import "@rails/actiontext"
+import $ from "jquery";
 
-// ページが表示されたときにアラームを出す
-document.addEventListener('DOMContentLoaded', () => {
-    window.alert('DOM LOADED')
-})
+// .article_titleをクリックするとアラームが表示される
+document.addEventListener("DOMContentLoaded", () => {
+    $('.article_title').on('click', () => {
+        window.alert('CLICKED')
+    })
+});
