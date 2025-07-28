2c8c66c4261c55d0d0db538641b5cc76fae5d0c0
HAYASHInnn <fffm572752@gmail.com>
2025-07-28 14:33:40 +0900

    ハートの処理を別ファイルに分割

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,6 +1,10 @@
 import $ from "jquery";
 import axios from "axios";
 import Rails from "@rails/ujs";
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent,
+} from "./modules/handle_heart";
 
 axios.defaults.headers.common["X-CSRF-Token"] = Rails.csrfToken();
 
@@ -60,33 +64,6 @@ document.addEventListener("turbo:load", () => {
     handleHeartDisplay(hasLiked);
   });
 
-  $(".inactive-heart").on("click", () => {
-    axios
-      .post(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === "ok") {
-          $(".active-heart").removeClass("hidden");
-          $(".inactive-heart").addClass("hidden");
-        }
-      })
-      .catch((e) => {
-        window.alert("Error");
-        console.log(e);
-      });
-  });
-
-  $(".active-heart").on("click", () => {
-    axios
-      .delete(`/articles/${articleId}/like`)
-      .then((response) => {
-        if (response.data.status === "ok") {
-          $(".inactive-heart").removeClass("hidden");
-          $(".active-heart").addClass("hidden");
-        }
-      })
-      .catch((e) => {
-        window.alert("Error");
-        console.log(e);
-      });
-  });
+  listenInactiveHeartEvent(articleId);
+  listenActiveHeartEvent(articleId);
 });
