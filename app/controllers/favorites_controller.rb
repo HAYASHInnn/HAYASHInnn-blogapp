69dc0ce22e54ec5ae6d667342b48d5b04430c1ba
HAYASHInnn <fffm572752@gmail.com>
2025-07-31 12:56:47 +0900

    module

--- app/controllers/favorites_controller.rb
+++
@@ -1,8 +0,0 @@
-class FavoritesController < ApplicationController
-    # ログイン必須
-    before_action :authenticate_user!
-
-    def index
-        @articles = current_user.favorite_articles
-    end
-end
